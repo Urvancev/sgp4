3af76da7ceaa9f7bdf147f38a602060b8f6755ab
Urvancev <s.urvancev@gmail.com>
2019-11-02 08:13:32 +0300

    Compile sattrack with VS command promt

---
+++ compile_sattrack.bat
@@ -0,0 +1,9 @@
+cl  /EHsc sattrack\sattrack.cc libsgp4\CoordGeodetic.cc libsgp4\CoordTopocentric.cc libsgp4\DateTime.cc libsgp4\DecayedException.cc libsgp4\Eci.cc libsgp4\Globals.cc libsgp4\Observer.cc libsgp4\OrbitalElements.cc^
+    libsgp4\SGP4.cc^
+    libsgp4\SatelliteException.cc^
+    libsgp4\SolarPosition.cc^
+    libsgp4\TimeSpan.cc^
+    libsgp4\Tle.cc^
+    libsgp4\TleException.cc^
+    libsgp4\Util.cc^
+    libsgp4\Vector.cc 
